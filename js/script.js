1726ff04b96477f39ffb65402d0a513a30129c14
Artur Galeev <galeev168@gmail.com>
2024-03-23 17:17:42 +0300

    fix timer

--- js/script.js
+++ js/script.js
@@ -39,13 +39,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     // Timer
 
-    const deadline = '2024-03-30'
+    const deadline = '2024-03-31'
     function getTimeRemaining(endtime) {
+
+        let days, hours, minutes, seconds
+
         const t = Date.parse(endtime) - Date.parse(new Date())
-        days = Math.floor(t / (1000 * 60 * 60 * 24))
-        hours = Math.floor((t / (1000 * 60 * 60) % 24))
-        minutes = Math.floor((t / 1000 / 60) % 60)
-        seconds = Math.floor((t / 1000) % 60)
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24))
+            hours = Math.floor((t / (1000 * 60 * 60) % 24))
+            minutes = Math.floor((t / 1000 / 60) % 60)
+            seconds = Math.floor((t / 1000) % 60)
+        }
 
     return {
         'total': t,
